2ce47033cf808a27282534bc754d12294dd025e0
Jacob Chacko <philosopherjake@msn.com>
2025-08-13 22:56:38 -0600

    Test

---
+++ Core/Boot/Grub.cs
@@ -0,0 +1,20 @@
+﻿using FluffyByte.MUDServer.Utilities;
+
+namespace FluffyByte.MUDServer.Core.Boot
+{
+    public class  Grub
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Scribe.Write(LogLevel.Info, "Test");
+            }
+
+            Scribe.Info("Info");
+            Scribe.Debug("Debug");
+            Scribe.Warn("Warn");
+            Scribe.Error("Error");
+        }
+    }
+}
---
+++ Core/ConsoleIO/IInput.cs
@@ -0,0 +1,26 @@
+﻿namespace FluffyByte.MUDServer.Core.ConsoleIO
+{
+    /// <summary>
+    /// An interface for reading input from a source, such as a console or network stream.
+    /// </summary>
+    public interface IInput
+    {
+        /// <summary>
+        /// Asynchronously reads the next line of text from the input stream.
+        /// </summary>
+        /// <remarks>This method reads a line of text terminated by a newline character or the end of the
+        /// input stream.  The returned string does not include the newline character.</remarks>
+        /// <param name="ct">A <see cref="CancellationToken"/> that can be used to cancel the operation.</param>
+        /// <returns>A <see cref="ValueTask{TResult}"/> representing the asynchronous operation.  The result contains the next
+        /// line of text, or <see langword="null"/> if the end of the input stream is reached.</returns>
+        ValueTask<string?> ReadLineAsync(CancellationToken ct = default);
+
+        /// <summary>
+        /// Attempts to read a single line of text.
+        /// </summary>
+        /// <param name="line">When this method returns, contains the line of text read, or <see langword="null"/> if no line was
+        /// available.</param>
+        /// <returns><see langword="true"/> if a line of text was successfully read; otherwise, <see langword="false"/>.</returns>
+        bool TryReadLine(out string? line);
+    }
+}
