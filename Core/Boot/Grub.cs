32be11c58804e55a93f1c96cc3c420ec24f1dea1
Jacob Chacko <philosopherjake@msn.com>
2025-08-15 09:27:25 -0600

    Extracted to new solutions

--- Core/Boot/Grub.cs
+++ Core/Boot/Grub.cs
@@ -2,7 +2,9 @@
 using System.Text;
 using System.Text.Unicode;
 using System.Threading.Tasks;
-using FluffyByte.MUDServer.Core.FileIO;
+using FluffyByte.FileIO;
+using FluffyByte.Logger;
+
 using FluffyByte.MUDServer.Utilities;
 
 namespace FluffyByte.MUDServer.Core.Boot
@@ -21,6 +23,31 @@ namespace FluffyByte.MUDServer.Core.Boot
             Scribe.Warn("Warn");
             Scribe.Error("Error");
 
+            CancellationToken ct = new();
+
+            FluffyFile? temp = FileWizard.LoadFluffyFile(@"E:\Temp\test.txt", FileAccess.ReadWrite, createIfMissing: true, cancellationToken: ct).Result;
+
+            if(temp is not null)
+            {
+                Scribe.Info("Loaded file.");
+                Scribe.Info($"Path: {temp.Path}");
+                Scribe.Info($"Mode: {temp.Mode}");
+                Scribe.Info($"Last Modified: {File.GetLastWriteTime(temp.Path)}");
+
+
+                byte[]? contents = FileWizard.ReadAllBytesAsync(temp, ct).Result;
+
+                if (contents is not null)
+                {
+                    Scribe.Info("File contents read successfully.");
+                    Scribe.Info($"Contents Length: {contents.Length} bytes");
+                    Scribe.Info($"Scribe Contents: {Encoding.UTF8.GetString(contents)}");
+                }
+            }
+            else
+            {
+                Scribe.Error("Failed to load file.");
+            }
         }
     }
 }
--- Core/FileIO/FluffyFile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FluffyByte.MUDServer.Core.FileIO
-{
-    public sealed record FluffyFile(string Path,
-        FileMode Mode = FileMode.OpenOrCreate,
-        FileAccess Access = FileAccess.ReadWrite,
-        FileShare Share = FileShare.Read,
-        int BufferSize = 8192,
-        FileOptions Options = FileOptions.Asynchronous | FileOptions.SequentialScan) : IFluffyFile;
-}
